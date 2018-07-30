f5a979c92e67fc970124fd91aa7b324eae939132
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2018-07-30 11:57:07 +0700

    Update opencv2_opencv3.py

--- opencv2_opencv3.py
+++ opencv2_opencv3.py
@@ -1,5 +1,13 @@
-mog = cv2.BackgroundSubtractorMOG(history=50, nmixtures=3, backgroundRatio=0.8)
+# sudo pip3 install opencv-contrib-python
 if imutils.is_cv2():
   mog = cv2.BackgroundSubtractorMOG(history=50, nmixtures=3, backgroundRatio=0.8)
 elif imutils.is_cv3():
   mog = cv2.bgsegm.createBackgroundSubtractorMOG(history=50, nmixtures=3, backgroundRatio=0.8)
+  
+ 
+ if imutils.is_cv2():
+      contours, _ = cv2.findContours(
+          mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  elif imutils.is_cv3():
+      _, contours, _ = cv2.findContours(
+          mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
