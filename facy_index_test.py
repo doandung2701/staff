8b98f0f8b25f1e267535467d1d42413681c1080e
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2018-07-31 12:25:52 +0700

    Update facy_index_test.py

--- facy_index_test.py
+++ facy_index_test.py
@@ -1,11 +1,16 @@
 h,w = (10,10)
 floodFill_mask = np.zeros((h+2, w+2), np.uint8)
 floodFill_mask[1:-1,1:-1][4,4] = 1
+floodFill_mask[1:-1,1:-1][4,5] = 1
 predict_time = np.ones((h,w), dtype='uint8')
+predict_time[2,1:4]=5
+predict_time[4,4]=10
+len(predict_time[predict_time > 1])
 expand_window = (3,3,5,5)
 expand_topleft_x, expand_topleft_y, expand_w, expand_h = expand_window
 predict_time[expand_topleft_y:expand_topleft_y+expand_h, expand_topleft_x:expand_topleft_x+expand_w]\
   [floodFill_mask[1:-1,1:-1][expand_topleft_y:expand_topleft_y+expand_h, expand_topleft_x:expand_topleft_x+expand_w] == 1] +=3
 predict_time
+len(predict_time[predict_time > 1])
  
  
