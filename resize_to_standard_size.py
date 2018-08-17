343c5e4f7211e0a8d2c55d6e1c80f85f3c42f2e6
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2018-08-17 07:35:07 +0700

    Update resize_to_standard_size.py

--- resize_to_standard_size.py
+++ resize_to_standard_size.py
@@ -13,12 +13,14 @@ def create_img(img_resolution, is_value_is_zero = True):
 	else:
 		img = np.ones((height,width,3), np.uint8)
 	return img
-def resize_img(img, cvt_area):
+def resize_img(img, cvt_area, standard_h=None):
     width, height = _img_revolution = img.shape[1::-1]
     area = width*height
     ratio = math.sqrt(1.0*cvt_area/area)
     cvt_width = int(width*ratio)
     cvt_height = int(height*ratio)
+    if standard_h is not None:
+        cvt_height = standard_h
     resize_shape = (cvt_width, cvt_height)
     resized_img = cv2.resize(img, resize_shape)
     return resized_img
@@ -29,6 +31,7 @@ def window_to_slice(window, slice_step=None):
 	else:
 		w_slice_step, h_slice_step = slice_step, slice_step
 		window_slice = slice(top, top+h, h_slice_step), slice(left,left+w, w_slice_step)
+        
 	return window_slice
 def resize(img_path, standard_size):
     standard_w, standard_h = standard_size
@@ -39,7 +42,7 @@ def resize(img_path, standard_size):
     ratio = 1.0*resized_h/img_h
     resized_w = int(ratio*img_w)
     if resized_w <= standard_w:
-        resized_img = resize_img(img, resized_w*resized_h)
+        resized_img = resize_img(img, resized_w*resized_h, standard_h = standard_h)
         window = (standard_w - resized_w)//2, 0, resized_w, resized_h
         standard_img[window_to_slice(window)] = resized_img
     else:
@@ -54,7 +57,7 @@ if __name__ == '__main__':
     try:
         size_str = sys.argv[2]
     except:
-        size_str = '300x360'
+        size_str = '360x188'
     standard_size = tuple(map(int,size_str.split('x')))
     img = cv2.imread(img_path)
     standard_img = resize(img_path, standard_size)
