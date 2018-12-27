4db59eeb277d9844843095d7054c664746d2bdef
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2018-12-28 01:01:32 +0700

    Update donggoi.sh

--- donggoi.sh
+++ donggoi.sh
@@ -1,12 +1,11 @@
 project='zemcy_eye'
 cd ..
-tar -cvf $project.tar  $project
+tar -cvhf $project.tar  $project
 folder=$project'_release'
 rm -rf $folder
 mkdir $folder
 mv $project.tar $folder
 cd $folder
-rm -rf $project
 tar -xvf $project.tar
 rm $project.tar
 cd $project
