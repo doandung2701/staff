bf61071dc028591c0769de545a2c4ce476c9192f
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2018-12-28 01:02:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+apt update
+apt install sudo
+sudo apt update && apt install -y libsm6 libxext6 libxrender-dev
+sudo apt-get install libglib2.0-0
+
+sudo apt install virtualenv
 virtualenv -p python3 p3env
 #source p3env/bin/activate to use this python
 ./p3env/bin/pip3 install -r requirements.txt
-./p3env/bin/pip3 install -i https://test.pypi.org/simple/ zemcy
+./p3env/bin/pip3 install -i https://test.pypi.org/simple/ zemcy==0.0.8
 sudo apt-get install curl
 curl -o src/videostream.py https://raw.githubusercontent.com/cuongvomanh/staff/master/videostream.py
-curl -o vni-full-standard.zip https://drive.google.com/file/d/1pnh_2rx53O4LEe364bN2mmZ6qPyAnDxo/view?usp=sharing
-sudo apt-get install unzip
-rm -rf vni-full-standard.zip 
-unzip vni-full-standard.zip
+
+# rm -f vni-full-standard.zip 
+# curl -o vni-full-standard.zip https://drive.google.com/file/d/1pnh_2rx53O4LEe364bN2mmZ6qPyAnDxo/view?usp=sharing
+# sudo apt-get install unzip
+# unzip vni-full-standard.zip
+# rm -f vni-full-standard.zip 
 ./see.sh
