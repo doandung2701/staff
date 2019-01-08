c139148062cdba77d1c22ac3b17e89bb044c8b30
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2019-01-08 22:59:27 +0700

    Create ubuntu_setup.sh

---
+++ ubuntu_setup.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get install terminator
+sudo apt install virtualenv
+sudo apt install net-tools
+sudo apt-get install openssh-server
+sudo apt-get install git-core
+sudo apt install python3-pip
+
+
+sudo apt-get install tesseract-ocr
+sudo apt-get install tesseract-ocr-vie
+
+
+sudo apt-get install ibus-unikey
+ibus restart
