b5e206745c6355d3fae771098676c5bcdf21e6ff
cuongvomanh <cuong.vomanh195@gmail.com>
2019-02-20 09:07:35 +0700

    undo to test

--- code/Librarys/compare/compare.py
+++ code/Librarys/compare/compare.py
@@ -1,13 +1,16 @@
 import filecmp
-def main(folder1, folder2, folder3):
-    comparison = filecmp.dircmp(folder1, folder3)
-    comparison.report_full_closure()
+def compare_two(folder1, folder2):
+    comparison = filecmp.dircmp(folder1, folder2)
+    # comparison.report_full_closure()
+    # print(comparison.common_files)
+    # print(comparison.diff_files)
+    # print(comparison.left_only)
+    print(comparison.right_only)
+    # return 
 if __name__=='__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument("--folder1", help="folder1")
     ap.add_argument("--folder2", help="folder2")
-    ap.add_argument("--folder3", help="folder3")
     args= vars(ap.parse_args())
-    print(args)
-    main(args["folder1"], args["folder2"], args["folder3"])
+    compare_two(args["folder1"], args["folder2"])
