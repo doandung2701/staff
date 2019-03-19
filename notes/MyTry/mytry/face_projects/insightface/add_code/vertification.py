e10e8162a4f1bd7c997c748b81b2cdba8dbfe8a3
cuongvomanh <cuong.vomanh195@gmail.com>
2019-03-19 21:31:28 +0700

    update

--- notes/MyTry/mytry/face_projects/insightface/add_code/vertification.py
+++ notes/MyTry/mytry/face_projects/insightface/add_code/vertification.py
@@ -1,6 +1,26 @@
-import numpy as np
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import argparse
+import sys
+import numpy as np
+from scipy import misc
+from sklearn.model_selection import KFold
+from scipy import interpolate
+import sklearn
+import cv2
+import math
+import datetime
+import pickle
+from sklearn.decomposition import PCA
+import mxnet as mx
+from mxnet import ndarray as nd
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+import face_image
+
+
 
 def evaluate(embeddings, issame_list):
 
