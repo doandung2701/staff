da62f4882513a40e05f91d5221060760a766c004
cuongvomanh <cuong.vomanh195@gmail.com>
2019-03-22 13:44:13 +0700

    update

--- notes/MyTry/mytry/face_projects/insightface/add_code/main.py
+++ notes/MyTry/mytry/face_projects/insightface/add_code/main.py
@@ -8,7 +8,7 @@ import cv2, pickle
 def identify(data, ide_model, vector_dir, k, output):
 	args = get_config()
 	face_model = FaceModel(args)
-	data_items = data.items()
+	data_items = data.items()[:3]
 	top_5s = []
 	for name, paths in data_items:
 		path = paths[0]
