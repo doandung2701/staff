e3f7214a1a856ae667d83798116619e62e8b73f9
cuongvomanh <cuong.vomanh195@gmail.com>
2019-03-24 19:05:37 +0700

    update

--- notes/MyTry/mytry/face_projects/insightface/add_code/main_v2.py
+++ notes/MyTry/mytry/face_projects/insightface/add_code/main_v2.py
@@ -70,7 +70,7 @@ def identify(tree, ide_model, known_vector_dir, k, output, threshold, batch_size
 		is_novelty = False
 		for i, (person, is_same) in enumerate(zip(test_img.candidates(), is_sames)):
 			if i == 0:
-				if is_same == True:
+				if is_same == 1:
 					top_5[i] = person.idx()
 				else:
 					top_5[i] = 1000
