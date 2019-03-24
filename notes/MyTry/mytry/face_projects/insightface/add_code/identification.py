1989ca29a565d5ea52c79e5d734746dc65644806
cuongvomanh <cuong.vomanh195@gmail.com>
2019-03-24 17:54:52 +0700

    update

--- notes/MyTry/mytry/face_projects/insightface/add_code/identification.py
+++ notes/MyTry/mytry/face_projects/insightface/add_code/identification.py
@@ -33,7 +33,7 @@ class Person:
 
 class TestImage(Image):
 	def __init__(self, path, emb=None):
-		super(self.__class__, self).__init__()
+		super(self.__class__, self).__init__(path, emb)
 		self._candidates = []
 		self._dists = []
 	
