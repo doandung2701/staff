38d5f4ef7e584c1f1831507e485b1a2ce0896ebe
cuongvomanh <cuong.vomanh195@gmail.com>
2019-03-25 10:34:11 +0700

    update

--- notes/MyTry/mytry/face_projects/insightface/add_code/data.py
+++ notes/MyTry/mytry/face_projects/insightface/add_code/data.py
@@ -59,7 +59,7 @@ def load_emb(data_dir, data, vector_dir):
 				else:
 					pdb.set_trace()
 					_img = cv2.resize(_img, (112,112))
-				# pdb.set_trace()
+				pdb.set_trace()
 				
 				_emb = fmodel.get_feature(_img)
 				with open(emb_path, 'wb') as f:
