0911cce53522641f531e35bb2f99520accea414a
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2019-06-14 08:36:31 +0700

    Update setup.sh

--- notes/azure/gw/setup.sh
+++ notes/azure/gw/setup.sh
@@ -1,6 +1,11 @@
 sudo apt update
 sudo apt install zip
 sudo apt install openjdk-8-jdk
+sudo apt install subversion
+svn export https://github.com/cuongvomanh/staff/trunk/notes/azure/gw
+mv gw tmp
+unzip EInvoiceGW.zip
+cd EInvoiceGW
 vim ../tmp/env
 source ../tmp/env
 bash ../tmp/env_setup.sh
