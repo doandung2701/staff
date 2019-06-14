497dc9956b6ca88c2ca2740bbc6204063be907e5
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2019-06-14 11:48:01 +0700

    Update env_setup.sh

--- notes/azure/reportgw/env_setup.sh
+++ notes/azure/reportgw/env_setup.sh
@@ -1,2 +1,4 @@
 sed -i -e 's/10.60.108.210/'$publicIPReportGW'/g' conf/report_config.properties
+sed -i -e 's/10.60.108.210/'$publicIPReportGW'/g' etc/config.properties
+sed -i -e 's/10.60.108.210/'$publicIPReportGW'/g' etc/ReportGW.conf
 sed -i -e 's:/u01/hddt:'$HOME':g' conf/report_config.properties
