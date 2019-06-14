e05e21139dbb924c5cb9898cd8a77176b132b2ce
Võ Mạnh Cường <cuong.vomanh195@gmail.com>
2019-06-14 08:37:19 +0700

    Update setup.sh

--- notes/azure/web/setup.sh
+++ notes/azure/web/setup.sh
@@ -4,6 +4,8 @@ sudo apt install openjdk-8-jdk
 sudo apt install subversion
 svn export https://github.com/cuongvomanh/staff/trunk/notes/azure/web
 mv web tmp
+unzip tomcat9999_azure_search.zip
+cd tomcat9999_azure_search
 vim ../tmp/env
 source ../tmp/env
 bash ../tmp/env_setup.sh
